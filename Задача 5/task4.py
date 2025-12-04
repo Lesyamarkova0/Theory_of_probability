b17f48bf56eaf72ce58c16752e94e30ad5009515
lesya <olesyamarkova270405@gmail.com>
2025-12-04 17:06:56 +0300

    Update task4.py

--- Задача 5/task4.py
+++ Задача 5/task4.py
@@ -9,9 +9,7 @@ X = np.arange(0, n + 1)
 geom_dist = hypergeom(M=N, n=M, N=n)
 probabilities = geom_dist.pmf(X)
 
-df = pd.DataFrame(data=probabilities.reshape(1, -1),
-                 columns = X,
-                 index=['p'])
+df = pd.DataFrame(data=probabilities.reshape(1, -1), columns = X, index=['p'])
 df
 
 print(f'Мат. ожидание = {geom_dist.mean():.4f}')
@@ -19,11 +17,14 @@ print(f'Дисперсия = {geom_dist.var():.4f}')
 print(f'Среднее квадратическое отклонение = {geom_dist.std():.4f}')
 
 max_prob = probabilities.max()
-mode_index = np.argmax(probabilities)
-mode = X[mode_index]
+mode = [int(x) for x in X if round(probabilities[x - 1], 6) == round(max_prob, 6)]
 print(f'Мода = {mode}')
 
-plt.figure(figsize=(4,2))
-
-plt.plot(X, probabilities, '-ro', ms=8, label = 'hypergeom pmf')
-plt.show();
+plt.figure(figsize=(8,3))
+plt.plot(X, probabilities, '-o', color = 'm')
+plt.title(f'Гипергеометрическое распределение')
+plt.xlabel('Число дефектных изделий, m')
+plt.ylabel('Вероятность, P(X=m)')
+plt.xticks(X)
+plt.grid(axis='y', alpha=0.75)
+plt.show()
